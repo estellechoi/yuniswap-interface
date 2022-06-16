ee0e07877a65be5baa2b0e23604faec232e17ee0
Estelle Choi <estele.choi@gmail.com>
2022-06-16 18:31:58 +0900

    chore: files linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import './App.css';
-import logo from './logo.svg';
+import './App.css'
+
+import logo from './logo.svg'
 
 function App() {
   return (
@@ -9,17 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
